b693b38a552dbb5feb5f6e8f5c3206a2a07371f3
jjezmarlo2 <67801314+jjezmarlo2@users.noreply.github.com>
2020-07-03 15:17:39 -0500

    Updated textbox.rb

--- docs/textbox.rb
+++ docs/textbox.rb
@@ -2,15 +2,49 @@ def extra_symbol
  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$"].sample
 end
 
-def file_include?(file, thing)
-	file = File.open(file, 'a+')
+def file_include?(file, thing, first_boolean)
+	filen = File.open(file, 'a+')
+	file_read = filen.read
+	file_array = file_read.split("\n")
+	if first_boolean == true
+		if file_array[0] == thing
+			return true
+		else
+			return false
+		end
+	else
+		if file_array.include?(thing)
+			return true
+		else
+			return false
+		end
+	end
+	filen.close
+end
+
+def add_someone(person, doc)
+	puts "#{person} added."
+	file = File.open(doc, 'a+')
+	file.puts person
+	file.close
+end
+def sub_someone(person, doc)
+	file = File.open(doc, 'a+')
 	file_read = file.read
 	file_array = file_read.split("\n")
-	if file_array.include?(thing)
-		return true
-	else
-		return false
+	puts file_array
+	puts ""
+	person_idx = file_array.find_index(person)
+	file_array.delete_at(person_idx)
+	puts file_array
+	file.close
+	file = File.open(doc, 'w')
+	file_array.each do |person_left|
+		file.puts person_left
 	end
+	
+	puts "#{person} subtracted."
+	file.close
 end
 
 def make_user_name(user_pw)
@@ -33,16 +67,36 @@ puts ">>> "
 file.close
 
 
-if @what_to_write == "contact manager"
-  file = File.open('contacts.txt','a+')
-  puts "What is your problem?: "
-  problem = gets.chomp
-  
-  file.puts "#{@user}: #{problem}"
-  
-  puts "Thank you for contacting us. We will get back to you as soon as we can."
-  file.close
-  
+if @what_to_write == "manage"
+	chat_people = chat_doc + "_people.txt"
+	if file_include?(chat_people, @user, true)
+		puts ""
+		puts "Would you like to add a person to your chat?:"
+		add_or_not = gets.chomp
+		if add_or_not == "yes"
+			puts "Person to add: "
+			person = gets.chomp
+			add_someone(person, chat_people)
+		end
+		puts ""
+		puts "Would you like to take away a person from your chat?: "
+		sub_or_not = gets.chomp
+		if sub_or_not == "yes"
+			puts "Person to take away: "
+			person = gets.chomp
+			sub_someone(person, chat_people)
+		end
+		puts ""
+		puts "Would you like to delete the current conversation?: "
+		delete_or_not = gets.chomp
+		if delete_or_not == "yes"
+			file = File.open(@real_chat,'w')
+			file.write
+		end
+	else
+		puts ""
+		puts "You are not authorized to manage this conversation. Please contact the creator of the chat."
+	end
 elsif @what_to_write == "see answers"
   file = File.open('replies.txt','a+')
   puts file.read
@@ -52,6 +106,7 @@ elsif @what_to_write == "r"
   in_home(person, chat_doc)
 elsif @what_to_write == "leave"
 elsif @what_to_write == "quit"
+
 else
   @real_chat = chat_doc + '.txt'
   file = File.open(@real_chat, 'a+')
@@ -63,43 +118,6 @@ end
 
 @pws = []
 
-def manager_is_in
-puts ""
-show_requests
-puts ""
-show_passwords
-puts ""
-ask_if_delete_conversation
-end
-
-def show_passwords
-file = File.open('PW.txt','a+') 
-puts "                     Here are the passwords!"
-puts ""
-file.each do |line|
-puts line
-@pws.push(line)
-end
-file.close
-puts "             Would you like to delete any passwords?: "
-puts ""
-puts ">>>"
-would_you_like = gets.chomp
-
-if would_you_like == "yes"
-  puts "              Which would you like to delete?: "
-  puts ""
-  puts ">>>"
-  which_to_delete = gets.chomp
-  file = File.open('PW.txt','w')
-  @pws.each do |pw|
-  unless pw == "#{which_to_delete}\n" || pw == "#{which_to_delete}"
-  file.write pw
-  end
-  end
-    file.close
-end
-end
 
 def ask_if_delete_conversation
 puts "                What chat would you like to see?"
@@ -124,49 +142,6 @@ else
 		file.write
 	end
 end
-end
-def show_requests
-  file = File.open('contacts.txt','a+')
-  puts "              Here are the requests, worthy manager!"
-  puts ""
-puts file.read
-file.close
-
-puts "                 Would you like to submit a reply?: "
-puts ">>>"
-submit_reply = gets.chomp
-
-if submit_reply == "yes"
-  puts "                     What is your reply?: "
-  puts ""
-  puts ">>>"
-  reply = gets.chomp
-  
-  file = File.open('replies.txt','a+')
-  file.puts reply
-  file.close
-  end
-puts "              Would you like to delete all requests?: "
-puts ">>>"
-delete_all = gets.chomp
-
-if delete_all == "yes"
-file = File.open('contacts.txt','w')
-file.write
-file.close
-end
-end
-
-def ask_if_delete_reply
-  puts ""
-  puts "Would you like to delete all replies? Please make sure they are all to you: "
-  delete_replies = gets.chomp
-  
-  if delete_replies == "yes"
-  file = File.open('replies.txt','w')
-  file.write
-  file.close
-  end
 end
 
 def open_how_to_use
@@ -197,10 +172,14 @@ puts "                   What will your username be?: "
 puts ">>>"
 username = gets.chomp
 password = username + extra_symbol
-puts "Your password is #{password}."
+puts "Your password is \"#{password}\""
 file = File.open('pw.txt','a+')
 file.puts password
 file.close
+puts "Type the honor word 'yes' to say that you know your password: "
+yes = gets.chomp
+if yes == "yes"
+end
 elsif new_or_not == "no"
 puts "                     What is your password?: "
 puts ">>>"
@@ -210,7 +189,7 @@ if supposed_password == "cookies.and.milk"
 manager_is_in
 end
 
-if file_include?('PW.txt', supposed_password)
+if file_include?('PW.txt', supposed_password, false)
 
 puts "            What chat would you like to go into?: "
 puts ">>>"
@@ -223,7 +202,8 @@ unless File.exist?(actual_doc)
 	person = "random_guy"
 	i=0
 	make_file_name = chat_doc + "_people.txt"
-	file = File.open(make_file_name, 'a+')
+	file = File.open(make_file_name, 'a+', false)
+	file.puts @you
 	until person === "done"
 		puts ""
 		person = gets.chomp
@@ -231,12 +211,11 @@ unless File.exist?(actual_doc)
 			file.puts person
 		end
 	end
-	file.puts @you
 	file.close
 end
 chat_people_name = chat_doc + "_people.txt"
 @chat_people_doc = File.open(chat_people_name, 'a+')
-unless file_include?(@chat_people_doc, @you)
+unless file_include?(@chat_people_doc, @you, false)
 	puts "You are not authorized to be part of this conversation."
 else
 puts ""
